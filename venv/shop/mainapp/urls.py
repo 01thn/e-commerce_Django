c9925d390d2cb38e9a0082a0922269d5a3d4a17b
thn <tikhon.trans@gmail.com>
2021-09-06 13:24:19 +0300

    Cart customize

--- venv/shop/mainapp/urls.py
+++ venv/shop/mainapp/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('category/<str:slug>/', views.CategoryDetailView.as_view(),name='category_detail'),
     path('cart/', views.CartView.as_view(),name='cart'),
     path('add-to-cart/<str:ct_model>/<str:slug>/', views.AddToCartView.as_view(),name='add_to_cart'),
-    path('remove-from-cart/<str:ct_model>/<str:slug>/', views.DeleteFromCartView.as_view(),name='remove_from_cart')
+    path('remove-from-cart/<str:ct_model>/<str:slug>/', views.DeleteFromCartView.as_view(),name='remove_from_cart'),
+    path('change-qty/<str:ct_model>/<str:slug>/', views.ChangeQtyView.as_view(),name='change_qty')
 ]
